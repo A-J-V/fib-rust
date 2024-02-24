ce276fe12dc97d8fcb780da94e3cba694e59b435
AJV <alexander.valarus@gmail.com>
2024-02-24 13:12:17 -0500

    More pyo3 magic

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+mod fib_calcs;
+use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
+use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
+
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
@@ -11,5 +15,7 @@ fn say_hello() {
 #[pymodule]
 fn test_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(say_hello));
+    m.add_wrapped(wrap_pyfunction!(fibonacci_number));
+    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     Ok(())
 }
